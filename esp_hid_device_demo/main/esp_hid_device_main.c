8ef7764d78f14ffe6c3cf4c475d4841923f6ae75
barryblueice <barryblueice@outlook.com>
2026-04-13 12:35:29 +0800

    尝试使用鼠标desc

--- esp_hid_device_demo/main/esp_hid_device_main.c
+++ esp_hid_device_demo/main/esp_hid_device_main.c
@@ -20,6 +20,8 @@
 #include "esp_hidd.h"
 #include "esp_hid_gap.h"
 
+#define REPORTID_MOUSE 0x01
+
 static const char *TAG = "HID_DEV_DEMO";
 
 typedef struct
@@ -32,70 +34,43 @@ typedef struct
 
 static local_param_t s_ble_hid_param = {0};
 
-const unsigned char mediaReportMap[] = {
-    0x05, 0x0C,        // Usage Page (Consumer)
-    0x09, 0x01,        // Usage (Consumer Control)
-    0xA1, 0x01,        // Collection (Application)
-    0x85, 0x03,        //   Report ID (3)
-    0x09, 0x02,        //   Usage (Numeric Key Pad)
-    0xA1, 0x02,        //   Collection (Logical)
-    0x05, 0x09,        //     Usage Page (Button)
-    0x19, 0x01,        //     Usage Minimum (0x01)
-    0x29, 0x0A,        //     Usage Maximum (0x0A)
-    0x15, 0x01,        //     Logical Minimum (1)
-    0x25, 0x0A,        //     Logical Maximum (10)
-    0x75, 0x04,        //     Report Size (4)
-    0x95, 0x01,        //     Report Count (1)
-    0x81, 0x00,        //     Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,              //   End Collection
-    0x05, 0x0C,        //   Usage Page (Consumer)
-    0x09, 0x86,        //   Usage (Channel)
-    0x15, 0xFF,        //   Logical Minimum (-1)
-    0x25, 0x01,        //   Logical Maximum (1)
-    0x75, 0x02,        //   Report Size (2)
-    0x95, 0x01,        //   Report Count (1)
-    0x81, 0x46,        //   Input (Data,Var,Rel,No Wrap,Linear,Preferred State,Null State)
-    0x09, 0xE9,        //   Usage (Volume Increment)
-    0x09, 0xEA,        //   Usage (Volume Decrement)
-    0x15, 0x00,        //   Logical Minimum (0)
-    0x75, 0x01,        //   Report Size (1)
-    0x95, 0x02,        //   Report Count (2)
-    0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x09, 0xE2,        //   Usage (Mute)
-    0x09, 0x30,        //   Usage (Power)
-    0x09, 0x83,        //   Usage (Recall Last)
-    0x09, 0x81,        //   Usage (Assign Selection)
-    0x09, 0xB0,        //   Usage (Play)
-    0x09, 0xB1,        //   Usage (Pause)
-    0x09, 0xB2,        //   Usage (Record)
-    0x09, 0xB3,        //   Usage (Fast Forward)
-    0x09, 0xB4,        //   Usage (Rewind)
-    0x09, 0xB5,        //   Usage (Scan Next Track)
-    0x09, 0xB6,        //   Usage (Scan Previous Track)
-    0x09, 0xB7,        //   Usage (Stop)
-    0x15, 0x01,        //   Logical Minimum (1)
-    0x25, 0x0C,        //   Logical Maximum (12)
-    0x75, 0x04,        //   Report Size (4)
-    0x95, 0x01,        //   Report Count (1)
-    0x81, 0x00,        //   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x09, 0x80,        //   Usage (Selection)
-    0xA1, 0x02,        //   Collection (Logical)
-    0x05, 0x09,        //     Usage Page (Button)
-    0x19, 0x01,        //     Usage Minimum (0x01)
-    0x29, 0x03,        //     Usage Maximum (0x03)
-    0x15, 0x01,        //     Logical Minimum (1)
-    0x25, 0x03,        //     Logical Maximum (3)
-    0x75, 0x02,        //     Report Size (2)
-    0x81, 0x00,        //     Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,              //   End Collection
-    0x81, 0x03,        //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,              // End Collection
+const uint8_t hid_report_descriptor[] = {
+
+    0x05, 0x01,                         // USAGE_PAGE (Generic Desktop)
+    0x09, 0x02,                         // USAGE (Mouse)
+    0xa1, 0x01,                         // COLLECTION (Application)
+    0x85, REPORTID_MOUSE,               // REPORT_ID
+    0x09, 0x01,                         // USAGE (Pointer)
+    0xa1, 0x00,  
+
+    0x05, 0x09,                         // USAGE_PAGE (Button)
+    0x19, 0x01,                         // USAGE_MINIMUM (Button 1)
+        0x29, 0x03,                     // USAGE_MAXIMUM (Button 3)
+        0x15, 0x00,                     // LOGICAL_MINIMUM (0)
+        0x25, 0x01,                     // LOGICAL_MAXIMUM (1)
+        0x75, 0x01,                     // REPORT_SIZE (1)
+        0x95, 0x03,                     // REPORT_COUNT (3)
+        0x81, 0x02,                     // INPUT (Data,Var,Abs)
+        0x95, 0x05,                     // REPORT_COUNT (5)
+        0x81, 0x03,                     // INPUT (Cnst,Var,Abs)
+
+        0x05, 0x01,                     // USAGE_PAGE (Generic Desktop)
+        0x09, 0x30,                     // USAGE (X)
+        0x09, 0x31,                     // USAGE (Y)
+        0x15, 0x81,                     // LOGICAL_MINIMUM (-127)
+        0x25, 0x7f,                     // LOGICAL_MAXIMUM (127)
+        0x75, 0x08,                     // REPORT_SIZE (8)
+        0x95, 0x02,                     // REPORT_COUNT (2)
+        0x81, 0x06,                     // INPUT (Data,Var,Rel)
+    0xC0,                               // END_COLLECTION (Physical)
+    0xC0,                               // END_COLLECTION (Application)
+
 };
 
 static esp_hid_raw_report_map_t ble_report_maps[] = {
     {
-        .data = mediaReportMap,
-        .len = sizeof(mediaReportMap)
+        .data = hid_report_descriptor,
+        .len = sizeof(hid_report_descriptor)
     }
 };
 
@@ -113,17 +88,6 @@ static esp_hid_device_config_t ble_hid_config = {
 void ble_hid_demo_task(void *pvParameters)
 {
     while (1) {
-        ESP_LOGI(TAG, "Send the volume");
-        // if (send_volum_up) {
-        //     esp_hidd_send_consumer_value(HID_CONSUMER_VOLUME_UP, true);
-        //     vTaskDelay(100 / portTICK_PERIOD_MS);
-        //     esp_hidd_send_consumer_value(HID_CONSUMER_VOLUME_UP, false);
-        // } else {
-        //     esp_hidd_send_consumer_value(HID_CONSUMER_VOLUME_DOWN, true);
-        //     vTaskDelay(100 / portTICK_PERIOD_MS);
-        //     esp_hidd_send_consumer_value(HID_CONSUMER_VOLUME_DOWN, false);
-        // }
-        // send_volum_up = !send_volum_up;
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
