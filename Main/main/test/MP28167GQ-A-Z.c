fc9c5895a6c08262dd2f65df1cf2e4a9a6818e9f
barryblueice <barryblueice@outlook.com>
2026-04-14 17:57:23 +0800

    更新MP28167测试代码

--- Main/main/test/MP28167GQ-A-Z.c
+++ Main/main/test/MP28167GQ-A-Z.c
@@ -3,11 +3,12 @@
 #include "driver/i2c_master.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 
 static const char *TAG = "MP28167_APP";
 
-#define I2C_MASTER_SCL_IO           36      
-#define I2C_MASTER_SDA_IO           35      
+#define I2C_MASTER_SCL_IO           18
+#define I2C_MASTER_SDA_IO           17
 #define MP28167_ADDR                0x60    
 
 #define REG_VREF_L                  0x00
@@ -15,13 +16,14 @@ static const char *TAG = "MP28167_APP";
 #define REG_VREF_GO                 0x02
 #define REG_IOUT_LIM                0x03
 
+
 esp_err_t mp28167_set_vref_mv(i2c_master_dev_handle_t dev, uint16_t mv) {
-    // 1. 公式转换: V_int = Vref / 0.8
+
     uint16_t v_int = (uint16_t)((mv / 0.8f) + 0.5f);
     if (v_int > 2047) v_int = 2047;
 
     uint8_t v_h = (uint8_t)((v_int >> 3) & 0xFF);
-    uint8_t v_l = (uint8_t)(v_int & 0x07);
+    uint8_t v_l = (uint8_t)(v_int & 0x07)
 
     uint8_t data_l[] = {REG_VREF_L, v_l};
     uint8_t data_h[] = {REG_VREF_H, v_h};
@@ -32,8 +34,11 @@ esp_err_t mp28167_set_vref_mv(i2c_master_dev_handle_t dev, uint16_t mv) {
     ESP_ERROR_CHECK(i2c_master_transmit(dev, data_go, 2, -1));
 
     ESP_LOGI(TAG, "VREF set to %d mV (Raw: 0x%03X)", mv, v_int);
+
     return ESP_OK;
+
 }
+
 
 float mp28167_get_vref_mv(i2c_master_dev_handle_t dev) {
     uint8_t addr_h = REG_VREF_H;
@@ -45,9 +50,16 @@ float mp28167_get_vref_mv(i2c_master_dev_handle_t dev) {
 
     uint16_t v_raw = ((uint16_t)val_h << 3) | (val_l & 0x07);
     return v_raw * 0.8f;
+
 }
 
+
 void app_main(void) {
+
+    // gpio_set_direction(14, GPIO_MODE_OUTPUT_OD);
+
+    // gpio_set_level(14, 0);
+
     i2c_master_bus_config_t bus_cfg = {
         .clk_source = I2C_CLK_SRC_DEFAULT,
         .i2c_port = 0,
@@ -55,6 +67,7 @@ void app_main(void) {
         .sda_io_num = I2C_MASTER_SDA_IO,
         .glitch_ignore_cnt = 7,
     };
+
     i2c_master_bus_handle_t bus_handle;
     ESP_ERROR_CHECK(i2c_new_master_bus(&bus_cfg, &bus_handle));
 
@@ -63,17 +76,23 @@ void app_main(void) {
         .device_address = MP28167_ADDR,
         .scl_speed_hz = 100000,
     };
+
     i2c_master_dev_handle_t dev_handle;
     ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
 
-    // mp28167_set_vref_mv(dev_handle, 600);
+    mp28167_set_vref_mv(dev_handle, 400);
     vTaskDelay(pdMS_TO_TICKS(2000));
 
+    // gpio_set_level(14, 1);
+
     while (1) {
+
         float current_vref = mp28167_get_vref_mv(dev_handle);
         ESP_LOGI(TAG, "Current VREF in chip: %.2f mV", current_vref);
-
         // mp28167_set_vref_mv(dev_handle, 200);
-        // vTaskDelay(pdMS_TO_TICKS(2000));
+        vTaskDelay(pdMS_TO_TICKS(2000));
+
     }
+
 }
+
