75a08cf2031b1067314a04ff3c5a98e87d123a66
barryblueice <barryblueice@outlook.com>
2026-04-14 17:52:42 +0800

    基于bluedroid协议栈的PTP测试

---
+++ ble_hid_device_demo/main/hid_dev.h
@@ -0,0 +1,42 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+#ifndef HID_DEV_H__
+#define HID_DEV_H__
+
+#include "hidd_le_prf_int.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct
+{
+  uint16_t    handle;           // Handle of report characteristic
+  uint16_t    cccdHandle;       // Handle of CCCD for report characteristic
+  uint8_t     id;               // Report ID
+  uint8_t     type;             // Report type
+  uint8_t     mode;             // Protocol mode (report or boot)
+} hid_report_map_t;
+
+typedef struct
+{
+  uint32_t    idleTimeout;      // Idle timeout in milliseconds
+  uint8_t     hidFlags;         // HID feature flags
+
+} hid_dev_cfg_t;
+
+void hid_dev_register_reports(uint8_t num_reports, hid_report_map_t *p_report);
+
+void hid_dev_send_report(esp_gatt_if_t gatts_if, uint16_t conn_id,
+                                    uint8_t id, uint8_t type, uint8_t length, uint8_t *data);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* HID_DEV_H__ */
