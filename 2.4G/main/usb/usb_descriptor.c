62e88b10643918557726dbae8181272bfdf9835f
barryblueice <barryblueice@outlook.com>
2026-04-16 23:19:54 +0800

    删除多余的mouse desc文件，添加dfu更新

--- 2.4G/main/usb/usb_descriptor.c
+++ 2.4G/main/usb/usb_descriptor.c
@@ -56,39 +56,6 @@ const uint8_t mouse_hid_report_descriptor[] = {
 
 };
 
-const uint8_t mouse_hid_report_descriptor[] = {
-
-    0x05, 0x01,                         // USAGE_PAGE (Generic Desktop)
-    0x09, 0x02,                         // USAGE (Mouse)
-    0xa1, 0x01,                         // COLLECTION (Application)
-    0x85, REPORTID_MOUSE,               // REPORT_ID
-    0x09, 0x01,                         // USAGE (Pointer)
-    0xa1, 0x00,  
-
-    0x05, 0x09,                         // USAGE_PAGE (Button)
-    0x19, 0x01,                         // USAGE_MINIMUM (Button 1)
-        0x29, 0x03,                     // USAGE_MAXIMUM (Button 3)
-        0x15, 0x00,                     // LOGICAL_MINIMUM (0)
-        0x25, 0x01,                     // LOGICAL_MAXIMUM (1)
-        0x75, 0x01,                     // REPORT_SIZE (1)
-        0x95, 0x03,                     // REPORT_COUNT (3)
-        0x81, 0x02,                     // INPUT (Data,Var,Abs)
-        0x95, 0x05,                     // REPORT_COUNT (5)
-        0x81, 0x03,                     // INPUT (Cnst,Var,Abs)
-
-        0x05, 0x01,                     // USAGE_PAGE (Generic Desktop)
-        0x09, 0x30,                     // USAGE (X)
-        0x09, 0x31,                     // USAGE (Y)
-        0x15, 0x81,                     // LOGICAL_MINIMUM (-127)
-        0x25, 0x7f,                     // LOGICAL_MAXIMUM (127)
-        0x75, 0x08,                     // REPORT_SIZE (8)
-        0x95, 0x02,                     // REPORT_COUNT (2)
-        0x81, 0x06,                     // INPUT (Data,Var,Rel)
-    0xC0,                               // END_COLLECTION (Physical)
-    0xC0,                               // END_COLLECTION (Application)
-
-};
-
 const uint8_t ptp_hid_report_descriptor[] = {
     
     //TOUCH PAD input TLC
