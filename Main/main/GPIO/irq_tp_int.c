7b5da337834f2a407de7306e37f4b0fa86d864e2
barryblueice <barryblueice@outlook.com>
2026-04-16 12:23:28 +0800

    INT由轮询改为中断触发

---
+++ Main/main/GPIO/irq_tp_int.c
@@ -0,0 +1,49 @@
+#include "driver/gpio.h"
+#include "esp_system.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+#include "esp_log.h"
+
+#include "I2C/i2c_hid.h"
+
+#define TAG "IRQ_TP_INT"
+
+static TaskHandle_t tp_task_handle = NULL;
+
+void tp_i2c_task(void *pvParameters) {
+    uint8_t packet[64]; 
+
+    while (1) {
+        if (ulTaskNotifyTake(pdTRUE, portMAX_DELAY)) {
+            
+            if ((i2c_master_receive(dev_handle, packet, 64, 100)) == ESP_OK) {
+                printf("Raw Data: ");
+                for(int i=0; i<64; i++) printf("%02x ", packet[i]);
+                printf("\n");
+            }
+        }
+    }
+}
+
+static void IRAM_ATTR gpio_isr_handler(void* arg) {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    vTaskNotifyGiveFromISR(tp_task_handle, &xHigherPriorityTaskWoken);
+    if (xHigherPriorityTaskWoken) {
+        portYIELD_FROM_ISR();
+    }
+}
+
+void irq_int_init(void) {
+    xTaskCreate(tp_i2c_task, "tp_task", 4096, NULL, 10, &tp_task_handle);
+
+    gpio_config_t io_conf = {
+        .intr_type = GPIO_INTR_NEGEDGE,
+        .pin_bit_mask = (1ULL << TP_INT_GPIO),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = 1,
+    };
+    gpio_config(&io_conf);
+    gpio_isr_handler_add(TP_INT_GPIO, gpio_isr_handler, NULL);
+}
--- Main/main/I2C/i2c_quene.c
+++ Main/main/I2C/i2c_quene.c
@@ -8,28 +8,4 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#define TAG "I2C_CMD"
-
-void read_touch_task(void *pvParameters)
-{
-    uint8_t packet[64];
-    ESP_LOGI(TAG, "Touch task started on Core 1");
-
-    while (1) {
-        if (gpio_get_level(TP_INT_GPIO) == 0) {
-
-            esp_err_t ret = i2c_master_receive(dev_handle, packet, 64, 100);
-            
-            if (ret == ESP_OK) {
-                if (ret == ESP_OK) {
-                    printf("Raw Data: ");
-                    for(int i=0; i<64; i++) printf("%02x ", packet[i]);
-                    printf("\n");
-                }
-            }
-
-        }
-        
-        vTaskDelay(pdMS_TO_TICKS(10)); 
-    }
-}
+#define TAG "I2C_QUENE"
